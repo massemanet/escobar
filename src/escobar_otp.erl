1fcc25ec2139cb9a52f7c8c885081e71205749d0
mats cronqvist <masse@cronqvi.st>
2016-05-30 21:51:14 +0200

    filter out MFA

--- src/escobar_otp.erl
+++ src/escobar_otp.erl
@@ -13,15 +13,24 @@ base(Tail) ->
 
 go() ->
   Apps = get_apps(),
-  [lists:reverse(trane:wget_sax(base(A),fun sxm/2,[])) || A <- Apps].
+  get_mf(Apps).
+
+get_mf(Apps) ->
+  lists:append(
+    [lists:reverse(trane:wget_sax(base(A),fun sxm/2,[])) || A <- Apps]).
 
 sxm({tag,"li",Attrs},A) ->
   case proplists:get_value("title",Attrs) of
     undefined -> A;
     M ->
       case proplists:get_value("expanded",Attrs) of
-        "false" ->  [{sep,M}|A];
-        undefined -> [M|A]
+        "false" ->
+          [{m,M}|A];
+        undefined ->
+          case re:run(M,"[a-z][a-zA-Z0-9_]+-[0-9]+") of
+            {match,[{0,_}]} -> [{f,M}|A];
+            _ -> A
+          end
       end
   end;
 sxm(_,A) -> A.
